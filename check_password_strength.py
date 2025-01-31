d0df926351f0581ea33d5f090f85329dbb19ad58
Tanuj Bhatia <84853581+tanujbhatia24@users.noreply.github.com>
2025-01-31 18:53:28 +0530

    Update check_password_strength.py

--- check_password_strength.py
+++ check_password_strength.py
@@ -1,6 +1,6 @@
 import re
 
-# Giving a heads up to the user aboyt the Password criteria
+# Giving a heads up to the user about the Password criteria
 print("## Password must meet the following criteria: ##")
 print(" - At least 8 characters long")
 print(" - Contains at least one uppercase letter")
@@ -10,30 +10,30 @@ print(" - Contains at least one special character from the list [!, @, #, $, %]"
 
 def check_password_strength(password):
     
-    # Creating an empty list to store the missing criteria in entered Password
+    # Creating an empty list to store the missing criteria in the entered Password
     missing_criteria = []
 
-    # Checking for min Password length
+    # Checking for minimum length criteria in the entered Password
     if len(password) < 8:
         missing_criteria.append("Password must be at least 8 characters long.")
     
-    # Checking for Uppercase letters in Password
+    # Checking for Uppercase letters criteria in the entered Password
     if not re.search(r"[A-Z]", password):
         missing_criteria.append("Password must contain at least one uppercase letter.")
 
-    # Checking for Lowercase letter in Password        
+    # Checking for Lowercase letter criteria in the entered Password       
     if not re.search(r"[a-z]", password):
         missing_criteria.append("Password must contain at least one lowercase letter.")
     
-    # Checking for a Digit in Password
+    # Checking for a Digit criteria in the entered Password
     if not re.search(r"[0-9]", password):
         missing_criteria.append("Password must contain at least one digit (0-9).")
     
-    # Checking for a Special Character in Password
+    # Checking for a Special Character criteria in the entered Password
     if not re.search(r"[!@#$%]", password):
         missing_criteria.append("Password must contain at least one special character from the list [!, @, #, $, %].")
 
-    # Providing feedback to the user incase any criteria is missing from the entered Password    
+    # Providing feedback to the user if any criteria is missing from the entered Password    
     if missing_criteria:
         print("Weak: Password is missing the following criteria:")
         for criteria in missing_criteria:
